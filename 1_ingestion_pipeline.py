90273f10233011453c6d02881833e9ab10c7c86a
dotmebhanu <dotmebhanu@gmail.com>
2026-03-26 20:55:31 +0530

    revised

--- 1_ingestion_pipeline.py
+++ 1_ingestion_pipeline.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
 
 
 
-
+#d
 # documents = [
 #    Document(
 #        page_content="Google LLC is an American multinational corporation and technology company focusing on online advertising, search engine technology, cloud computing, computer software, quantum computing, e-commerce, consumer electronics, and artificial intelligence (AI).",
